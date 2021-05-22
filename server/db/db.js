565124c0359aef51d53635b989f11f529aeb5b71
Damien Outar <damienoutar@gmail.com>
2021-05-22 11:54:34 -0400

    set up db

---
+++ server/db/db.js
@@ -0,0 +1,7 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost/channel_4_news_db',
+  { logging: false }
+);
+
+module.exports = db;
