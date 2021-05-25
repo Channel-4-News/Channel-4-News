18f72a232fcdf3e2102499bec9de4c4f8fd766dc
Keri Weiss <keret.weiss@gmail.com>
2021-05-24 20:32:32 -0400

    created auth routes closes #15

---
+++ server/api/auth.js
@@ -0,0 +1,28 @@
+const { Router, json } = require('express');
+const router = Router();
+const {
+  models: { User },
+} = require('../db/models/associations');
+
+router.use(json());
+
+router.post('/', async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const token = await User.authenticate({ username, password });
+    res.send({ token });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/', async (req, res, next) => {
+  try {
+    const user = await User.byToken(req.headers.authorization);
+    res.send(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
--- server/api/router.js
+++ server/api/router.js
@@ -5,6 +5,8 @@ router.use('/users', require('./users'));
 router.use('/families', require('./families'));
 router.use('/chores', require('./chores'));
 router.use('/allowance', require('./allowance'));
+router.use('/auth', require('./auth'));
+
 // router.use('/wishlists', require('./wishlists'));
 // router.use('/transactions', require('./transactions'));
 
