38c2e99bf4906a9f4e5275b203c8c4311f4bbfd5
Keri Weiss <keret.weiss@gmail.com>
2021-05-25 22:28:16 -0400

    trying to fix travis error

--- server/api/router.js
+++ server/api/router.js
@@ -6,7 +6,7 @@ router.use('/families', require('./families'));
 router.use('/chores', require('./chores'));
 router.use('/allowance', require('./allowance'));
 router.use('/auth', require('./auth'));
-router.use('/wishlistItem', require('./wishlistItem'));
+router.use('/wishlistItem', require('./wishListItem'));
 // router.use('/transactions', require('./transactions'));
 
 module.exports = router;
