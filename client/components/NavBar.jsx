4a4f902ac0876960e20501a6ee0297f6b49aa776
Anna Litovskaya <Litovskaya09@gmail.com>
2021-05-27 23:32:10 -0400

    login and sign up links added. closes #32

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -3,8 +3,10 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
+import { HashRouter as Link } from 'react-router-dom';
 
-const NavBar = () => {
+const NavBar = (props) => {
+  const user = props.user.status;
   return (
     <div id="navbar-wrapper">
       <AppBar position="static" id="navbar">
@@ -13,8 +15,39 @@ const NavBar = () => {
             Channel-4-News
           </IconButton>
           <div id="navbar-links-wrapper">
-            <Button color="inherit">Log In</Button>
-            <Button color="inherit">Sign Up</Button>
+            {/* USER IS NOT SIGNED IN */}
+            {user === undefined ? (
+              <Button color="inherit" href="/#/login">
+                Log In
+              </Button>
+            ) : (
+              ''
+            )}
+            {user === undefined ? (
+              <Button color="inherit" href="/#/signup">
+                Sign Up
+              </Button>
+            ) : (
+              ''
+            )}
+
+            {/* USER IS A CHILD */}
+            {user === 'Child' ? <Button color="inherit">Wish List</Button> : ''}
+            {user === 'Child' ? <Button color="inherit">Chores</Button> : ''}
+            {user === 'Child' ? <Button color="inherit">Chat</Button> : ''}
+            {user === 'Child' ? <Button color="inherit">Settings</Button> : ''}
+            {user === 'Child' ? <Button color="inherit">Sign Out</Button> : ''}
+
+            {/* USER IS A PARENT */}
+            {user === 'Parent' ? (
+              <Button color="inherit">Notifications</Button>
+            ) : (
+              ''
+            )}
+            {user === 'Parent' ? <Button color="inherit">Chores</Button> : ''}
+            {user === 'Parent' ? <Button color="inherit">Chat</Button> : ''}
+            {user === 'Parent' ? <Button color="inherit">Settings</Button> : ''}
+            {user === 'Parent' ? <Button color="inherit">Sign Out</Button> : ''}
           </div>
         </Toolbar>
       </AppBar>
