fc14776ca87f0071b953dc6652eceb6c9fc0c7a2
Anna Litovskaya <Litovskaya09@gmail.com>
2021-05-27 22:59:28 -0400

    material ui replaces materialize

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,46 +1,24 @@
 import React from 'react';
-import { Navbar, NavItem, Icon } from 'react-materialize';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 
 const NavBar = () => {
   return (
-    <Navbar
-      alignLinks="right"
-      brand={
-        <a className="brand-logo" href="#">
-          Logo
-        </a>
-      }
-      id="mobile-nav"
-      menuIcon={<Icon>menu</Icon>}
-      options={{
-        draggable: true,
-        edge: 'left',
-        inDuration: 250,
-        onCloseEnd: null,
-        onCloseStart: null,
-        onOpenEnd: null,
-        onOpenStart: null,
-        outDuration: 200,
-        preventScrolling: true,
-      }}
-    >
-      <NavItem href="/#/signup">Sign Up</NavItem>
-      <NavItem href="/#/login">Log In</NavItem>
-      {/* {user === 'anonymous' ? <NavItem href="/#/signup">Sign Up</NavItem> : ''}
-      {user === 'anonymous' ? <NavItem href="/#/login">Log In</NavItem> : ''}
-      {user === 'parent' ? <NavItem href="/#/">Notifications</NavItem> : ''}
-      {user === 'parent' ? <NavItem href="/#/">Chores</NavItem> : ''}
-      {user === 'parent' ? <NavItem href="/#/">Chat</NavItem> : ''}
-      {user === 'parent' ? <NavItem href="/#/">Siblings</NavItem> : ''}
-      {user === 'parent' ? <NavItem href="/#/">Settings</NavItem> : ''}
-      {user === 'parent' ? <NavItem href="/#/">Sign Out</NavItem> : ''}
-      {user === 'child' ? <NavItem href="/#/">Wish List</NavItem> : ''}
-      {user === 'child' ? <NavItem href="/#/">Chores</NavItem> : ''}
-      {user === 'child' ? <NavItem href="/#/">Chat</NavItem> : ''}
-      {user === 'child' ? <NavItem href="/#/">Siblings</NavItem> : ''}
-      {user === 'child' ? <NavItem href="/#/">Settings</NavItem> : ''}
-      {user === 'child' ? <NavItem href="/#/">Sign Out</NavItem> : ''} */}
-    </Navbar>
+    <div id="navbar-wrapper">
+      <AppBar position="static" id="navbar">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            Channel-4-News
+          </IconButton>
+          <div id="navbar-links-wrapper">
+            <Button color="inherit">Log In</Button>
+            <Button color="inherit">Sign Up</Button>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 };
 
