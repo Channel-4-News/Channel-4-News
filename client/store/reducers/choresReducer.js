f8f18048d3cb7ed56adc0655f42ab017d0c6b1a9
Keri Weiss <keret.weiss@gmail.com>
2021-05-30 14:26:15 -0400

    parents can delete chore

--- client/store/reducers/choresReducer.js
+++ client/store/reducers/choresReducer.js
@@ -1,8 +1,10 @@
 import { ADD_CHORE } from '../actions/choreActions/addChore';
+import { DELETE_CHORE } from '../actions/choreActions/deleteChore';
 import { GET_CHORES } from '../actions/choreActions/fetchChoresByFamily';
 import { UPDATE_CHORE } from '../actions/choreActions/updateChore';
 
 const choresReducer = (state = [], action) => {
+  console.log(action);
   if (action.type === GET_CHORES) {
     return (state = action.chores);
   }
@@ -11,6 +13,9 @@ const choresReducer = (state = [], action) => {
   }
   if (action.type === UPDATE_CHORE) {
     return (state = action.chores);
+  }
+  if (action.type === DELETE_CHORE) {
+    return (state = state.filter((chore) => chore.id !== action.deletedId));
   }
   return state;
 };
