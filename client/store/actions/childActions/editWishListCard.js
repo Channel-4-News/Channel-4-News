ebc63c998cf3c98a02b793b2662f54e0e456ec2a
Anna Litovskaya <Litovskaya09@gmail.com>
2021-05-30 15:17:29 -0400

    you can edit wish list items. closes #154

---
+++ client/store/actions/childActions/editWishListCard.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const EDIT_WISH_LIST_CARD = 'EDIT_WISH_LIST_CARD';
+
+const editWishListCard = (wishListCard) => {
+  return async (dispatch) => {
+    try {
+      let { id } = wishListCard;
+      id = id * 1;
+      const newWishListItem = (
+        await axios.put(`/api/wishlistitem/${id}`, wishListCard)
+      ).data;
+      dispatch(_editWishListCard(newWishListItem));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+const _editWishListCard = (wishListItem) => {
+  return { type: EDIT_WISH_LIST_CARD, wishListItem };
+};
+
+export { EDIT_WISH_LIST_CARD, editWishListCard };
