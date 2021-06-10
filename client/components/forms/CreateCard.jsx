130c5ac57806f2e1c1a5e7f5bb3dc746b84bb46b
keriweiss <keret.weiss@gmail.com>
2021-06-10 17:05:27 -0400

    card customizer form styled

--- client/components/forms/CreateCard.jsx
+++ client/components/forms/CreateCard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 import {
   TextField,
@@ -9,11 +10,9 @@ import {
   Menu,
 } from '@material-ui/core';
 import Cards from 'react-credit-cards';
+import CardStepper from './createCard/CardStepper';
 
 const CreateCard = (props) => {
-  const [val, setVal] = useState('#939393');
-  const [anchorEl, setAnchorEl] = useState(null);
-  const [open, setOpen] = useState(false);
   const [backgroundColor, setBackgroundColor] = useState('');
   const [backgroundImage, setBackgroundImage] = useState('');
   const [cardBackground] = useState(
@@ -26,6 +25,16 @@ const CreateCard = (props) => {
     focus: '',
     name: '',
     number: '',
+  });
+  const [textFieldStyle, setTextFieldStyle] = useState({
+    width: '400px',
+    alignSelf: 'center',
+  });
+  const [colorPickerStyle, setColorPickerStyle] = useState({
+    display: 'none',
+  });
+  const [imagesStyle, setImagesStyle] = useState({
+    display: 'none',
   });
 
   useEffect(() => {
@@ -44,190 +53,209 @@ const CreateCard = (props) => {
     },
   }));
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+  const handleColorClick = (color) => {
+    setBackgroundColor(color);
+    axios.put(`/api/users/${props.currUser.id}`, { cardColor: color });
   };
 
-  const handleClose = () => {
-    setAnchorEl(null);
-    setOpen(false);
-  };
-
-  const handleClickOpen = () => {
-    setOpen(true);
+  const handleImageClick = (e) => {
+    setBackgroundImage(e.target.name);
+    axios.put(`/api/users/${props.currUser.id}`, {
+      cardImage: e.target.name,
+    });
   };
 
   const classes = useStyles();
   return (
-    <div>
-      <h4>CREATE YOUR CARD</h4>
-      <TextField
-        className={classes.root}
-        label="Full Name"
-        variant="outlined"
-        color="secondary"
-        onChange={(e) => {
-          setCardDetails({ ...cardDetails, name: e.target.value });
-        }}
+    <div id="createYourCard">
+      <div id="tempCardView">
+        <Cards
+          cvc={cardDetails.cvc}
+          expiry={cardDetails.expiry}
+          focused={cardDetails.focus}
+          name={cardDetails.name}
+          number={cardDetails.number}
+        />
+      </div>
+      <CardStepper
+        setInputStyle={setTextFieldStyle}
+        inputStyle={textFieldStyle}
+        setColorPickerStyle={setColorPickerStyle}
+        setImagesStyle={setImagesStyle}
+        history={props.history}
+        name={cardDetails.name}
       />
-      <div id="cardColorPicker">
-        <h5>Choose A Color</h5>
-        <Button
-          // color="secondary"
-          aria-controls="simple-menu"
-          aria-haspopup="true"
-          onClick={handleClick}
-          variant="contained"
-          style={{ backgroundColor: `${val}` }}
-        >
-          &nbsp;
-        </Button>
-        <Menu
-          id="simple-menu"
-          anchorEl={anchorEl}
-          keepMounted
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
-          <MenuItem
-            style={{ backgroundColor: '#3e98ff' }}
-            onClick={() => {
-              handleClose();
-              setVal('#3e98ff');
-              setBackgroundColor('#3e98ff');
-            }}
+      <div id="cardChooser">
+        <TextField
+          style={textFieldStyle}
+          className={classes.root}
+          label="Full Name"
+          variant="outlined"
+          color="secondary"
+          onChange={(e) => {
+            setCardDetails({ ...cardDetails, name: e.target.value });
+          }}
+        />
+        <div id="cardColorPicker" style={colorPickerStyle}>
+          {/* <Button
+            // color="secondary"
+            aria-controls="simple-menu"
+            aria-haspopup="true"
+            onClick={handleClick}
+            variant="contained"
+            style={{ backgroundColor: `${val}` }}
           >
             &nbsp;
-          </MenuItem>
-          <MenuItem
-            onClick={() => {
-              handleClose;
-              setVal('#ff73ff');
-              setBackgroundColor('#ff73ff');
-            }}
+          </Button> */}
+          <Button
+            // color="secondary"
+            aria-controls="simple-menu"
+            aria-haspopup="true"
+            // onClick={handleClick}
+            name="#3e98ff"
+            test="tomatoe"
+            onClick={() => handleColorClick('#3e98ff')}
+            variant="contained"
+            style={{ backgroundColor: '#3e98ff' }}
+          >
+            &nbsp;
+          </Button>
+          <Button
+            // color="secondary"
+            aria-controls="simple-menu"
+            aria-haspopup="true"
+            // onClick={handleClick}
+            onClick={() => handleColorClick('rgb(255, 72, 0)')}
+            variant="contained"
+            style={{ backgroundColor: 'rgb(255, 72, 0)' }}
+          >
+            &nbsp;
+          </Button>
+          <Button
+            // color="secondary"
+            aria-controls="simple-menu"
+            aria-haspopup="true"
+            // onClick={handleClick}
+            onClick={() => handleColorClick('#ff73ff')}
+            variant="contained"
             style={{ backgroundColor: '#ff73ff' }}
           >
             &nbsp;
-          </MenuItem>
-          <MenuItem
-            onClick={() => {
-              handleClose;
-              setVal('#fffc49');
-              setBackgroundColor('#fffc49');
-            }}
+          </Button>
+          <Button
+            // color="secondary"
+            aria-controls="simple-menu"
+            aria-haspopup="true"
+            // onClick={handleClick}
+            onClick={() => handleColorClick('#fffc49')}
+            variant="contained"
             style={{ backgroundColor: '#fffc49' }}
           >
             &nbsp;
-          </MenuItem>
-          <MenuItem
-            onClick={() => {
-              handleClose;
-              setVal('#ff7300');
-              setBackgroundColor('#ff7300');
-            }}
-            style={{ backgroundColor: '#ff7300' }}
-          >
-            &nbsp;
-          </MenuItem>
-          <MenuItem
-            onClick={() => {
-              handleClose;
-              setVal('#00d9ff');
-              setBackgroundColor('#00d9ff');
-            }}
+          </Button>
+          <Button
+            // color="secondary"
+            aria-controls="simple-menu"
+            aria-haspopup="true"
+            // onClick={handleClick}
+            onClick={() => handleColorClick('#00d9ff')}
+            variant="contained"
             style={{ backgroundColor: '#00d9ff' }}
           >
             &nbsp;
-          </MenuItem>
-          <MenuItem
-            onClick={() => {
-              handleClose;
-              setVal('#00ff6a');
-              setBackgroundColor('#00ff6a');
-            }}
+          </Button>
+          <Button
+            // color="secondary"
+            aria-controls="simple-menu"
+            aria-haspopup="true"
+            // onClick={handleClick}
+            onClick={() => handleColorClick('#00ff6a')}
+            variant="contained"
             style={{ backgroundColor: '#00ff6a' }}
           >
             &nbsp;
-          </MenuItem>
-        </Menu>
-      </div>
-      <div>
-        <h5>Choose An Image</h5>
-        <div id="cardImageSelector">
-          <img
-            src="public/images/cardIcons/basketball.png"
-            name="basketball"
-            onClick={(e) => setBackgroundImage(e.target.name)}
-          />
-          <img
-            src="public/images/cardIcons/bathdog.png"
-            name="bathdog"
-            onClick={(e) => setBackgroundImage(e.target.name)}
-          />
-          <img
-            src="public/images/cardIcons/bernie.png"
-            name="bernie"
-            onClick={(e) => setBackgroundImage(e.target.name)}
-          />
-          <img
-            src="public/images/cardIcons/catFace.png"
-            name="catFace"
-            onClick={(e) => setBackgroundImage(e.target.name)}
-          />
-          <img
-            src="public/images/cardIcons/dogFace.png"
-            name="dogFace"
-            onClick={(e) => setBackgroundImage(e.target.name)}
-          />
-          <img
-            src="public/images/cardIcons/earth.png"
-            name="earth"
-            onClick={(e) => setBackgroundImage(e.target.name)}
-          />
-          <img
-            src="public/images/cardIcons/flowerpower.png"
-            name="flowerpower"
-            onClick={(e) => setBackgroundImage(e.target.name)}
-          />
-          <img
-            src="public/images/cardIcons/panda.png"
-            name="panda"
-            onClick={(e) => setBackgroundImage(e.target.name)}
-          />
-          <img
-            src="public/images/cardIcons/penguinpaper.png"
-            name="penguinpaper"
-            onClick={(e) => setBackgroundImage(e.target.name)}
-          />
-          <img
-            src="public/images/cardIcons/pizzapizza.png"
-            name="pizzapizza"
-            onClick={(e) => setBackgroundImage(e.target.name)}
-          />
-          <img
-            src="public/images/cardIcons/spiderman.png"
-            name="spiderman"
-            onClick={(e) => setBackgroundImage(e.target.name)}
-          />
-          <img
-            src="public/images/cardIcons/sushi.png"
-            name="sushi"
-            onClick={(e) => setBackgroundImage(e.target.name)}
-          />
-          <img
-            src="public/images/cardIcons/tacotaco.png"
-            name="tacotaco"
-            onClick={(e) => setBackgroundImage(e.target.name)}
-          />
+          </Button>
+        </div>
+        <div id="imageSelectContainer" style={imagesStyle}>
+          <div id="cardImageSelector">
+            <img
+              src="public/images/cardIcons/basketball.png"
+              name="basketball"
+              onClick={handleImageClick}
+            />
+            <img
+              src="public/images/cardIcons/bathdog.png"
+              name="bathdog"
+              onClick={handleImageClick}
+            />
+            <img
+              src="public/images/cardIcons/bernie.png"
+              name="bernie"
+              onClick={handleImageClick}
+            />
+            <img
+              src="public/images/cardIcons/catpeak.png"
+              name="catpeak"
+              onClick={handleImageClick}
+            />
+            <img
+              src="public/images/cardIcons/dogFace.png"
+              name="dogFace"
+              onClick={handleImageClick}
+            />
+            <img
+              src="public/images/cardIcons/earth.png"
+              name="earth"
+              onClick={handleImageClick}
+            />
+            <img
+              src="public/images/cardIcons/donut.png"
+              name="donut"
+              onClick={handleImageClick}
+            />
+            <img
+              src="public/images/cardIcons/orangutanstand.png"
+              name="orangutanstand"
+              onClick={handleImageClick}
+            />
+            <img
+              src="public/images/cardIcons/flowerpower.png"
+              name="flowerpower"
+              onClick={handleImageClick}
+            />
+            <img
+              src="public/images/cardIcons/panda.png"
+              name="panda"
+              onClick={handleImageClick}
+            />
+            <img
+              src="public/images/cardIcons/penguinpaper.png"
+              name="penguinpaper"
+              onClick={handleImageClick}
+            />
+            <img
+              src="public/images/cardIcons/pizzapizza.png"
+              name="pizzapizza"
+              onClick={handleImageClick}
+            />
+            <img
+              src="public/images/cardIcons/spiderman.png"
+              name="spiderman"
+              onClick={handleImageClick}
+            />
+            <img
+              src="public/images/cardIcons/sushi.png"
+              name="sushi"
+              onClick={handleImageClick}
+            />
+            <img
+              src="public/images/cardIcons/tacotaco.png"
+              name="tacotaco"
+              onClick={handleImageClick}
+            />
+          </div>
         </div>
       </div>
-      <Cards
-        cvc={cardDetails.cvc}
-        expiry={cardDetails.expiry}
-        focused={cardDetails.focus}
-        name={cardDetails.name}
-        number={cardDetails.number}
-      />
     </div>
   );
 };
