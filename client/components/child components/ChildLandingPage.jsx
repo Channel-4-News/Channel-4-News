df0235326def2d6ab508582339a2545d3a0ea8db
Damien Outar <damienoutar@gmail.com>
2021-06-14 20:23:55 -0400

    withdrawal logic added

--- client/components/child components/ChildLandingPage.jsx
+++ client/components/child components/ChildLandingPage.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import SpendingGraph from './SpendingGraph';
+import { connect } from 'react-redux';
 import { Avatar } from '@material-ui/core';
 import VirtualCard from '../forms/VirtualCard';
-import { connect } from 'react-redux';
 
 class ChildLandingPage extends React.Component {
   constructor() {
     super();
+    this.categorizeTransactions = this.categorizeTransactions.bind(this);
+  }
+  categorizeTransactions() {
+    let transactionsObject = {};
+    this.props.user.transactions.map((transaction) => {
+      if (!transactionsObject[transaction.category]) {
+        transactionsObject[transaction.category] = parseInt(transaction.cost);
+      } else {
+        transactionsObject[transaction.category] += parseInt(transaction.cost);
+      }
+    });
+    let total = 0;
+    for (const [key, value] of Object.entries(transactionsObject)) {
+      total += value;
+    }
+    let transactionsArray = [];
+    for (const [key, value] of Object.entries(transactionsObject)) {
+      let temp = {};
+      temp[key] = [value, ((value / total) * 100).toFixed(2)];
+      transactionsArray.push(temp);
+    }
+    return transactionsArray;
   }
 
   render() {
+    let transactions = this.categorizeTransactions();
     return this.props.user.allowance ? (
       <div id="childLandingPage">
         <p>Hello, {this.props.user.firstName}!</p>
