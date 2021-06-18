f47d2cd70f6a537376d8e8f15d25f2509f6ff3e9
Damien Outar <damienoutar@gmail.com>
2021-06-17 20:10:51 -0400

    modifiying allowance upon refresh

--- client/components/parentComponents/ChildCard.jsx
+++ client/components/parentComponents/ChildCard.jsx
@@ -38,8 +38,110 @@ const usePersonStyles = makeStyles(() => ({
     fontSize: '0.75rem',
   },
 }));
+const useModalStyles = makeStyles((props) => ({
+  body: {
+    fontFamily: 'Arial, Helvetica, sans-serif',
+    fontSize: '17px',
+    lineHeight: 1.6,
+  },
+  modal: (props) => ({
+    display: props.display,
+    position: 'fixed',
+    zIndex: 1,
+    left: 0,
+    top: 0,
+    height: '100%',
+    width: '100%',
+    overflow: 'auto',
+    backgroundColor: 'rbga(0,0,0,0.5)',
+  }),
+  '@keyframes modalopen': {
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+  },
+  modalContent: {
+    backgroundColor: '#f4f4f4',
+    margin: '20% auto',
+    padding: '20px',
+    width: '70%',
+    boxShadow: '0 5px 8px 0 rgba(0,0,0,0.2),0 7px 20px 0 rgba(0,0,0,0.17)',
+    animation: '$modalopen 1s',
+  },
+  closeBtn: {
+    color: '#ccc',
+    float: 'right',
+    fontSize: '30px',
+    '&:hover': {
+      color: '#000',
+      textDecoration: 'none',
+      cursor: 'pointer',
+    },
+    '&focus': {
+      color: '#000',
+      textDecoration: 'none',
+      cursor: 'pointer',
+    },
+  },
+}));
 
-const PersonItem = ({ src, name, balance, selectedKid }) => {
+const ModalItem = ({ kid }) => {
+  const [display, setDisplay] = useState('none');
+  const [newAllowance, setNewAllowance] = useState(kid.allowance);
+  const [newInterval, setNewInterval] = useState(kid.allowanceInterval);
+  let props = { display: display };
+  const styles = useModalStyles(props);
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    console.log(kid);
+    await axios.put(`/api/users/allowance/modify/${kid.id}`, {
+      newAllowance,
+      newInterval,
+    });
+    setDisplay('none');
+    console.log('submitted');
+  };
+  const handleChange = (evt) => {
+    evt.target.name === 'allowance'
+      ? setNewAllowance(evt.target.value)
+      : setNewInterval(evt.target.value);
+  };
+  return (
+    <div>
+      <Button
+        id="modalBtn"
+        className={styles.btn}
+        variant={'outlined'}
+        onClick={() => {
+          setDisplay('block');
+          console.log(props);
+        }}
+      >
+        Change Allowance
+      </Button>
+      <div id="simpleModal" className={cx(styles.modal)}>
+        <div className={cx(styles.modalContent)}>
+          <span
+            className={cx(styles.closeBtn)}
+            onClick={() => {
+              setDisplay('none');
+            }}
+          >
+            &times;
+          </span>
+          <h1>Adjust Allowance</h1>
+          <form onSubmit={handleSubmit}>
+            <h6>New Allowance:</h6>
+            <input type="text" name="allowance" onChange={handleChange}></input>
+            <h6>New Allowance Interval:</h6>
+            <input type="text" name="interval" onChange={handleChange}></input>
+            <button type="submit">Save Changes</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+const PersonItem = ({ src, name, balance, selectedKid, kid }) => {
   const avatarStyles = useDynamicAvatarStyles({ size: 100 });
   const styles = usePersonStyles();
   return (
@@ -55,7 +157,10 @@ const PersonItem = ({ src, name, balance, selectedKid }) => {
           </div>
         </Item>
         <Item position={'middle'}>
-          <div>Allowance</div>
+          <div>
+            Allowance in {kid.allowanceInterval} days: ${kid.allowance}
+          </div>
+          <ModalItem kid={kid} />
           <AliasLink to={{ pathname: '/chores', state: { selectedKid } }}>
             <Button className={styles.btn} variant={'outlined'}>
               Assigned Chores
@@ -117,10 +222,10 @@ const ChildCard = React.memo(function SocialCard(props) {
           <Item stretched className={styles.headline}>
             Your kids:
           </Item>
-          <Item className={styles.actions}>
+          {/* <Item className={styles.actions}>
             <Link className={styles.link}>Refresh</Link> •{' '}
             <Link className={styles.link}>See all</Link>
-          </Item>
+          </Item> */}
         </Row>
         {props.kids.map((kid) => {
           return (
@@ -130,6 +235,7 @@ const ChildCard = React.memo(function SocialCard(props) {
                 balance={kid.balance}
                 src={kid.imgUrl}
                 selectedKid={kid}
+                kid={kid}
               />
               <SpendingGraph transactions={kid.transactions} />
               <Divider variant={'middle'} className={styles.divider} />
